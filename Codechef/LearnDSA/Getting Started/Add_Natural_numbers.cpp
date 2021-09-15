5a2b5d09ec8a389ba446b8cf82d1f84b845858be
Aryan Agrawal <90298650+Rainbow-IQ@users.noreply.github.com>
2021-09-16 01:01:57 +0530

    Create Add_Natural_numbers.cpp

---
+++ Codechef/LearnDSA/Getting Started/Add_Natural_numbers.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define debug(x) cerr << #x << " = " << x << endl;
+const int mod = 1000000007;
+using namespace std;
+const char nll = '\n';
+#define ll long long
+/*
+TODO: Think clearly about the problem.
+* The Problem sometimes is easier than what you think
+! Always see the constraints
+* Donot give up! You can do this!
+*/
+void solve(){
+    ll n;
+    cin>>n;
+    cout<<((n*(n+1))/2);
+}
+int main(){
+    ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+    int t = 1;
+    //cin>>t; //!may or maynot required
+    while(t--){
+        solve();
+    }
+    return 0;
+}
