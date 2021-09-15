62aac4dc0f38634d0a76a28179ff5d7a82631926
Aryan Agrawal <90298650+Rainbow-IQ@users.noreply.github.com>
2021-09-16 01:09:51 +0530

    Create Life_the_Universe_and_Everything.cpp

---
+++ Codechef/LearnDSA/Complexity Analysis and Basic Warmup/Life_the_Universe_and_Everything.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define debug(x) cerr << #x << " = " << x << endl;
+const int mod = 1000000007;
+using namespace std;
+const char nll = '\n';
+#define ll long long
+/*
+TODO: Think clearly about the problem.
+* The Problem sometimes is easier than what you think
+! Always see the constraints
+* Donot give up! You can do this!
+*/
+void solve(){
+    while(true){
+        int a;
+        cin>>a;
+        if(a!=42){
+            cout<<a<<nll;
+        }
+        else{
+            return;
+        }
+    }
+}
+int main(){
+    ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+    int t = 1;
+    //cin>>t; //!may or maynot required
+    while(t--){
+        solve();
+    }
+    return 0;
+}
