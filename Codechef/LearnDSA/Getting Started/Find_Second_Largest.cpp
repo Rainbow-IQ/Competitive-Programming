314ec87ff01bf847b0737f250602de41e960bb7a
Aryan Agrawal <90298650+Rainbow-IQ@users.noreply.github.com>
2021-09-16 00:58:13 +0530

    Create Find_Second_Largest.cpp

---
+++ Codechef/LearnDSA/Getting Started/Find_Second_Largest.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define debug(x) cerr << #x << " = " << x << endl;
+const int mod = 1000000007;
+using namespace std;
+const char nll = '\n';
+#define ll long long
+/*
+TODO: Think clearly about the problem.
+* The Problem sometimes is easier than what you think
+! Always see the constraints
+* Donot give up! You can do this!
+*/
+void solve(){
+    int arr[3];
+    int a,b,c;
+    cin>>a>>b>>c;
+    arr[0] = a;
+    arr[1] = b;
+    arr[2] = c;
+    sort(arr,arr+3);
+    cout<<arr[1];
+}
+int main(){
+    ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+    int t = 1;
+    //cin>>t; //!may or maynot required
+    while(t--){
+        solve();
+    }
+    return 0;
+}
