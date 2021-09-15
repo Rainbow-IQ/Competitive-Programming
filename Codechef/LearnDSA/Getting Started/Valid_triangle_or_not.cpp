fdd3bfbfa5b35089317f2b27d6001a9a9c7c6406
Aryan Agrawal <90298650+Rainbow-IQ@users.noreply.github.com>
2021-09-16 01:00:44 +0530

    Create Valid_triangle_or_not.cpp

---
+++ Codechef/LearnDSA/Getting Started/Valid_triangle_or_not.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define debug(x) cerr << #x << " = " << x << endl;
+const int mod = 1000000007;
+using namespace std;
+const char nll = '\n';
+#define ll long long
+/*
+TODO: Think clearly about the problem.
+* The Problem sometimes is easier than what you think
+! Always see the constraints
+* Donot give up! You can do this!
+*/
+void solve(){
+    int a,b,c;
+    cin>>a>>b>>c;
+    if(a>0 && b>0 && c>0 && (a+b)>c && (b+c)>a && (a+c)>b){
+        cout<<"YES";
+    }
+    else{
+        cout<<"NO";
+    }
+}
+int main(){
+    ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+    int t = 1;
+    //cin>>t; //!may or maynot required
+    while(t--){
+        solve();
+    }
+    return 0;
+}
